aba6600752095e764b13023cd732811738633200
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2025-05-31 19:21:41 +0700

    Removed the swagger options

--- src/routes/swagger.ts
+++ src/routes/swagger.ts
@@ -6,17 +6,6 @@ import swaggerDocument from '../../swagger.json'
 const router = Router()
 
 router.use('/api-docs', swaggerUi.serve)
-router.get(
-  '/api-docs',
-  swaggerUi.setup(swaggerDocument, {
-    explorer: true,
-    swaggerOptions: {
-      requestInterceptor: (request: any) => {
-        request.headers.Origin = '*'
-        return request
-      }
-    }
-  })
-)
+router.get('/api-docs', swaggerUi.setup(swaggerDocument))
 
 export default router
